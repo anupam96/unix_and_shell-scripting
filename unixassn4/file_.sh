b9f43b5a08ec5202a681221723f210eaba4a4b9d
Vrishabh Sharma <vrishabhsharma22@gmail.com>
2016-09-24 02:09:33 +0530

    Update file_.sh

--- unixassn4/file_.sh
+++ unixassn4/file_.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ -d "$@" ]; then
-    echo "Number of files is $(find "$@" -type f | wc -l)"
-    echo "Number of directories is $(find "$@" -type d | wc -l)"
+    echo "Number of files are $(find "$@" -type f | wc -l)"
+    echo "Number of directories are $(find "$@" -type d | wc -l)"
 else
     echo "[ERROR]  No directory in the System."
     exit 1
